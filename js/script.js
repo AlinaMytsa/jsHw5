1a9f5337d26c9ceb6d697d915033124f8a47be5d
AlinaMytsa <act.mitsa@gmail.com>
2022-07-07 15:30:36 +0300

    init

---
+++ js/script.js
@@ -0,0 +1,23 @@
+'use strict';
+
+// task 1-2
+const user ={
+  // name: 'John',
+  surname: 'Smith',
+}
+
+user.name = 'Pete';
+
+console.log(user);
+
+
+// task3
+let salaries ={
+  John: 100,
+  Ann: 160,
+  Pete: 130,
+}
+
+let sum = salaries.John + salaries.Ann + salaries.Pete;
+
+console.log(sum);
